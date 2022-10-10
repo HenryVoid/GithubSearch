a6a824783bdddcb185edf28bb2c9b0a152104eb9
Henry <101556773+HenryVoid@users.noreply.github.com>
2022-10-10 17:33:19 +0900

    [#3] add: MainUseCase

--- GithubSearch/GithubSearch/Scenes/Main/MainUseCase.swift
+++ GithubSearch/GithubSearch/Scenes/Main/MainUseCase.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+import RxSwift
+
+enum MainUseCaseEvent {
+    case searchRepos(GitHubSearch.Response)
+    case errorMsg(String)
+}
+
+
+protocol MainUseCaseType {
+    var event: PublishSubject<MainUseCaseEvent> { get }
+    
+    func execute()
+}
+
+final class MainUseCase: MainUseCaseType {
+    
+    var event = PublishSubject<MainUseCaseEvent>()
+    
+    private let gitHubSearchRepository: GitHubSearchRepositoryType
+    
+    init(
+        gitHubSearchRepository: GitHubSearchRepositoryType
+    ) {
+        self.gitHubSearchRepository = gitHubSearchRepository
+    }
+    
+    func execute() {
+        self.searchRepo(page: 1, keyword: "RxSwift")
+    }
+}
+
+extension MainUseCase {
+    private func searchRepo(page: Int, keyword: String) {
+        self.gitHubSearchRepository
+            .search(
+                page: page,
+                keyword: keyword
+            ) { result, error in
+                guard let result = result else {
+                    self.event.onNext(.errorMsg("호출 에러"))
+                    return
+                }
+                
+                self.event.onNext(.searchRepos(result))
+            }
+    }
+}
